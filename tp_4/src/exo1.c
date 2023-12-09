d4b6f01adfcadb04d21c5c4696dbaee1a23c2569
guillaume356 <kleinguillaume005@gmail.com>
2023-12-09 14:08:55 +0100

    exercice1 tp docker explications des commandes + modification tp4

--- tp_4/src/exo1.c
+++ tp_4/src/exo1.c
@@ -8,24 +8,21 @@
 // atomiquement et correctement dans un contexte multithread ou de signal.
 volatile sig_atomic_t sigint_count = 0;
 
-// Définition de la fonction qui gère les signaux.
-// Cette fonction est appelée automatiquement lorsqu'un signal SIGINT ou SIGTERM est reçu.
-void signal_handler(int signal) {
-    if (signal == SIGINT) {
-        // Si le signal est SIGINT, incrémenter le compteur.
-        ++sigint_count;
-    } else if (signal == SIGTERM) {
-        // Si le signal est SIGTERM, afficher le nombre de SIGINT reçus et terminer le programme.
-        printf("Fin du programme. Nombre de SIGINT reçus : %d\n", sigint_count);
-        exit(0);
-    }
+void handle_sigint() {
+    sigint_count++;
+    printf("SIGINT reçu. Nombre de fois: %d\n", sigint_count);
+}
+
+void handle_sigterm() {
+    printf("SIGTERM reçu. Terminaison du programme.\n");
+    _exit(0);
 }
 
 int main() {
     printf("Le PID de ce processus est %d\n", getpid());
     // Associer le gestionnaire de signaux 'signal_handler' aux signaux SIGINT et SIGTERM.
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
+    signal(SIGINT, handle_sigint);
+    signal(SIGTERM, handle_sigterm);
 
     // Boucle infinie : le programme reste dans cette boucle jusqu'à ce qu'il reçoive SIGTERM.
     while (1) {
